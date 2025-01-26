eddb24c922f82f24ad80a253a77cb934cd5db52d
Elver <holahacker01@gmail.com>
2025-01-26 01:07:02 -0500

    Initial commit

---
+++ GitAvanzado/GitAvanzado/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitAvanzado
+//
+//  Created by Elver Mayta Hernández on 26/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitAvanzado/GitAvanzado/GitAvanzadoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitAvanzadoApp.swift
+//  GitAvanzado
+//
+//  Created by Elver Mayta Hernández on 26/01/25.
+//
+
+import SwiftUI
+
+@main
+struct GitAvanzadoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
