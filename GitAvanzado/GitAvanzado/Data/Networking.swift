d4145b11419386e5d10a86501b583f4fad48f9f9
Elver <holahacker01@gmail.com>
2025-01-26 01:14:25 -0500

    Add changes for feature/colaborador1

---
+++ GitAvanzado/GitAvanzado/Data/Networking.swift
@@ -0,0 +1,12 @@
+//
+//  Networking.swift
+//  GitAvanzado
+//
+//  Created by Elver Mayta Hernández on 26/01/25.
+//
+
+import Foundation
+
+class Networking {
+    
+}
---
+++ GitAvanzado/GitAvanzado/Domain/Model/UserModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserModel.swift
+//  GitAvanzado
+//
+//  Created by Elver Mayta Hernández on 26/01/25.
+//
+
+import Foundation
+
+struct UserModel {
+    var nombre: String
+    var apellidos: String
+}
